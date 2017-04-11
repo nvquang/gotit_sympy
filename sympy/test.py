523d23a46bbf4f88656df4c662da601fdd0b3333
nvquang <quang.nguyen2016@ict.jvn.edu.vn>
2017-04-11 18:20:47 +0700

    add code sympy

---
+++ sympy/test.py
@@ -0,0 +1,17 @@
+from __future__ import division
+from sympy import *
+x = symbols('x')
+# k, m, n = symbols('k m n', integer=True)
+# f, g, h = symbols('f g h', cls=Function)
+
+# a = 4*x*y*z + 3*y*x*z + 2*z*x*y + 2 + 3
+
+b = 2*x*x
+print("b: ", b)
+
+# print("Str expr: ", str_expr)
+# a = sympify(str_expr)
+# print("Result: ", a)
+
+
+# simplify(a)
